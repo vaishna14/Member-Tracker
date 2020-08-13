02d53f399c2700601be8183a73f7d4c09854940f
vaishna14 <vaishnavivaidya1@gmail.com>
2020-08-13 10:52:14 +0530

    Folder structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import Main from "./componnets/Board/Main";
+import Login from "./componnets/login/login";
+import Signup from "./componnets/login/signup";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+      <Route path="/" exact>
+        <Main />
+      </Route>
+      <Route path="/login" exact>
+        <Login />
+      </Route> <Route path="/signup" exact>
+        <Signup />
+      </Route>
+      <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/componnets/Board/Task.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Task() {
+    return (
+        <div>
+            Task
+        </div>
+    )
+}
+
+export default Task
